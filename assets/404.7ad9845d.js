41c23ad71bd81c9ff39f527a4059ba63d7afc53f
wangqi <wangqi_pitaya@163.com>
2022-05-15 21:54:04 +0800

    init

---
+++ assets/404.7ad9845d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.0081ba53.js";function t(){return r("div",{children:"404"})}export{t as default};
---
+++ assets/index.516f10cf.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.0081ba53.js";function t(){return o("div",{children:"Doc"})}export{t as default};
